ab98e223b641f39208eb9fd2e7426baa717ccd37
Liril <LirilColombia@gmail.com>
2020-09-10 20:33:53 -0500

    Envenenar liril
    liril envenenee++
    liril_emVemWSA++

--- js/index/main.js
+++ js/index/main.js
@@ -1,3 +1,631 @@
+parse= function(a, b){ 
+	switch(a[0]){ 
+  case "#": 
+     a= document.getElementById(a.slice(1, a.length)); 
+     break; 
+  case ".": 
+    a= document.getElementsByClassName(a.slice(1, a.length)); 
+    break; 
+	default: 
+  	a= document.getElementsByTagName(a); 
+    break; 
+	}; 
+     
+	b != -1 ? a[b].innerHTML = Cinnamon : a.innerHTML = Cinnamon; 
+}; 
+
+Accents= /*Were I able to access it*/{
+  "a": [ 
+    "a", 
+    "á", 
+    "à", 
+    "ă", 
+    "ắ", 
+    "ằ", 
+    "ẵ", 
+    "ẳ", 
+    "â", 
+    "ấ", 
+    "ầ", 
+    "ẫ", 
+    "ẩ", 
+    "ǎ", 
+    "å", 
+    "ǻ", 
+    "ä", 
+    "ǟ", 
+    "ã", 
+    "ȧ", 
+    "ǡ", 
+    "ą", 
+    "́ą́", 
+    "̃ą̃", 
+    "ā", 
+    "̀ā̀", 
+    "ả", 
+    "̋ȁ", 
+    "̋a̋", 
+    "ȃ", 
+    "ạ", 
+    "ặ", 
+    "ậ", 
+    "ḁ", 
+    "ⱥ", 
+    "ᶏ", 
+    "ẚ" 
+  ], 
+  "b": [ 
+    "b", 
+    "ḃ", 
+    "ḅ", 
+    "ḇ", 
+    "ƀ", 
+    "ɓ", 
+    "ꞗ", 
+    "ᵬ", 
+    "ᶀ" 
+  ],
+  "c": [ 
+    "c", 
+    "ć", 
+    "ĉ", 
+    "č", 
+    "ċ", 
+    "ç", 
+    "ḉ", 
+    "ȼ", 
+    "ꞓ", 
+    "ꞔ", 
+    "ƈ", 
+    "ɕ" 
+  ], 
+  "d": [ 
+    "d", 
+    "ď", 
+    "ḋ", 
+    "ḑ", 
+    "̦d̦", 
+    "ḍ", 
+    "ḓ", 
+    "ḏ", 
+    "đ", 
+    "ð", 
+    "ɖ", 
+    "ɗ", 
+    "ᵭ", 
+    "ᶁ", 
+    "ᶑ", 
+    "ȡ" 
+  ],
+  "e": [ 
+    "e", 
+    "é", 
+    "è", 
+    "ĕ", 
+    "ê", 
+    "ế", 
+    "ề", 
+    "ễ", 
+    "ể", 
+    "̄ê̄", 
+    "̌ê̌", 
+    "ě", 
+    "ë", 
+    "ẽ", 
+    "ė", 
+    "́ė́", 
+    "̃ė̃", 
+    "ȩ", 
+    "ḝ", 
+    "ę", 
+    "́ę́", 
+    "̃ę̃", 
+    "ē", 
+    "ḗ", 
+    "ḕ", 
+    "ẻ", 
+    "ȅ", 
+    "e̋", 
+    "ȇ", 
+    "ẹ", 
+    "ệ", 
+    "ḙ", 
+    "ḛ", 
+    "ɇ", 
+    "̩e̩", 
+    "̩è̩", 
+    "̩é̩", 
+    "ᶒ", 
+    "ⱸ", 
+    "ꬴ", 
+    "ꬳ" 
+  ], 
+  "f": [ 
+    "f", 
+    "ḟ", 
+    "ƒ", 
+    "ꞙ", 
+    "ᵮ", 
+    "ᶂ" 
+  ], 
+  "g": [ 
+    "g", 
+    "ǵ", 
+    "ğ", 
+    "ĝ", 
+    "ǧ", 
+    "ġ", 
+    "̃g̃", 
+    "ģ", 
+    "ḡ", 
+    "ǥ", 
+    "ꞡ", 
+    "ɠ", 
+    "ᶃ", 
+    "ꬶ" 
+  ], 
+  "h": [ 
+    "h", 
+    "ĥ", 
+    "ȟ", 
+    "ḧ", 
+    "ḣ", 
+    "ḩ", 
+    "ḥ", 
+    "ḫ", 
+    "̱ẖ", 
+    "ħ", 
+    "ⱨ", 
+    "ɦ", 
+    "ꞕ" 
+  ], 
+  "i": [ 
+    "i", 
+    "í", 
+    "i̇́", 
+    "ì", 
+    "i̇̀", 
+    "ĭ", 
+    "î", 
+    "ǐ", 
+    "ï", 
+    "ḯ", 
+    "ĩ", 
+    "i̇̃", 
+    "į", 
+    "į́", 
+    "į̇́", 
+    "į̃", 
+    "į̇̃", 
+    "ī", 
+    "ī̀", 
+    "ỉ", 
+    "ȉ", 
+    "i̋", 
+    "ȋ", 
+    "ị", 
+    "ḭ", 
+    "ɨ", 
+    "ᶖ", 
+    "i̇", 
+    "ı" 
+  ], 
+  "j": [ 
+    "j", 
+    "ĵ", 
+    "ɉ", 
+    "ǰ", 
+    "ʝ", 
+    "j̃", 
+    "j̇̃", 
+    "ɟ", 
+    "ʄ" 
+  ], 
+  "k": [ 
+    "k", 
+    "ḱ", 
+    "ǩ", 
+    "ķ", 
+    "ḳ", 
+    "ḵ", 
+    "ƙ", 
+    "ⱪ", 
+    "ᶄ", 
+    "ꝁ", 
+    "ꝃ", 
+    "ꝅ", 
+    "ꞣ" 
+  ], 
+  "l": [ 
+    "l", 
+    "ĺ", 
+    "ľ", 
+    "ļ", 
+    "ḷ", 
+    "ḹ", 
+    "̃l̃", 
+    "ḽ", 
+    "ḻ", 
+    "ł", 
+    "ŀ", 
+    "ƚ", 
+    "ꝉ", 
+    "ⱡ", 
+    "ɫ", 
+    "ɬ", 
+    "ꞎ", 
+    "ꬷ", 
+    "ꬸ", 
+    "ꬹ", 
+    "ᶅ", 
+    "ɭ", 
+    "ȴ" 
+  ], 
+  "m": [ 
+    "m", 
+    "ḿ", 
+    "̋m̋", 
+    "ṁ", 
+    "ṃ", 
+    "̃m̃", 
+    "ᵯ", 
+    "ᶆ", 
+    "ɱ", 
+    "ꬺ" 
+  ],
+  "n": [
+    "n", 
+    "ń", 
+    "ǹ", 
+    "ň", 
+    "ñ", 
+    "ṅ", 
+    "ņ", 
+    "ṇ", 
+    "ṋ", 
+    "ṉ", 
+    "̈n̈", 
+    "ɲ", 
+    "ŋ", 
+    "ƞ", 
+    "ꞑ", 
+    "ꞥ", 
+    "ᵰ", 
+    "ᶇ", 
+    "ɳ", 
+    "ȵ", 
+    "ꬻ", 
+    "ꬼ" 
+  ], 
+  "o": [ 
+    "o", 
+    "ó", 
+    "ò", 
+    "ŏ", 
+    "ô", 
+    "ố", 
+    "ồ", 
+    "ỗ", 
+    "ổ", 
+    "ǒ", 
+    "ö", 
+    "ȫ", 
+    "ő", 
+    "õ", 
+    "ṍ", 
+    "ṏ", 
+    "ȭ", 
+    "ȯ", 
+    "͘o͘", 
+    "ȱ", 
+    "ø", 
+    "ǿ", 
+    "ǫ", 
+    "ǭ", 
+    "ō", 
+    "ṓ", 
+    "ṑ", 
+    "ỏ", 
+    "ȍ", 
+    "ȏ", 
+    "ơ", 
+    "ớ", 
+    "ờ", 
+    "ỡ", 
+    "ở", 
+    "ợ", 
+    "ọ", 
+    "ộ", 
+    "̩o̩", 
+    "̩ò̩", 
+    "̩ó̩", 
+    "ɵ", 
+    "ꝋ", 
+    "ꝍ", 
+    "ⱺ" 
+  ], 
+  "p": [ 
+    "p", 
+    "ṕ", 
+    "ṗ", 
+    "ᵽ", 
+    "ꝑ", 
+    "ƥ", 
+    "ꝓ", 
+    "ꝕ", 
+    "̃p̃", 
+    "ᵱ", 
+    "ᶈ" 
+  ],
+  "q": [ 
+    "q", 
+    "ꝗ", 
+    "ꝙ", 
+    "ɋ", 
+    "ʠ" 
+  ], 
+  "r": [ 
+    "r", 
+    "ŕ", 
+    "ř", 
+    "ṙ", 
+    "ŗ", 
+    "ȑ", 
+    "ȓ", 
+    "ṛ", 
+    "ṝ", 
+    "ṟ", 
+    "̃r̃", 
+    "ɍ", 
+    "ꞧ", 
+    "ɽ", 
+    "ᵲ", 
+    "ᶉ", 
+    "ꭉ" 
+  ], 
+  "s": [ 
+    "s", 
+    "ś", 
+    "ṥ", 
+    "ŝ", 
+    "š", 
+    "ṧ", 
+    "̈s̈", 
+    "ṡ", 
+    "ş", 
+    "ṣ", 
+    "ṩ", 
+    "ș", 
+    "̩s̩", 
+    "ꞩ", 
+    "ȿ", 
+    "ʂ", 
+    "ᶊ", 
+    "ᵴ" 
+  ], 
+  "t": [ 
+    "t", 
+    "ť", 
+    "ṫ", 
+    "ţ", 
+    "ṭ", 
+    "ț", 
+    "ṱ", 
+    "ṯ", 
+    "ŧ", 
+    "ⱦ", 
+    "ƭ", 
+    "ʈ", 
+    "̈ẗ", 
+    "ᵵ", 
+    "ƫ", 
+    "ȶ" 
+  ], 
+  "u": [ 
+    "u", 
+    "ú", 
+    "ù", 
+    "ŭ", 
+    "û", 
+    "ǔ", 
+    "ů", 
+    "ü", 
+    "ǘ", 
+    "ǜ", 
+    "ǚ", 
+    "ǖ", 
+    "ű", 
+    "ũ", 
+    "ṹ", 
+    "ų", 
+    "́ų́", 
+    "̃ų̃", 
+    "ū", 
+    "ṻ", 
+    "̀ū̀", 
+    "́ū́", 
+    "̃ū̃", 
+    "ủ", 
+    "ȕ", 
+    "ȗ", 
+    "ư", 
+    "ứ", 
+    "ừ", 
+    "ữ", 
+    "ử", 
+    "ự", 
+    "ụ", 
+    "ṳ", 
+    "ṷ", 
+    "ṵ", 
+    "ʉ", 
+    "ᶙ", 
+    "ꭒ" 
+  ], 
+  "v": [ 
+    "v", 
+    "ṽ", 
+    "ṿ", 
+    "ꝟ", 
+    "ʋ", 
+    "ỽ", 
+    "ᶌ", 
+    "ⱱ", 
+    "ⱴ" 
+  ], 
+  "w": [ 
+    "w", 
+    "ẃ", 
+    "ẁ", 
+    "ŵ", 
+    "ẅ", 
+    "ẇ", 
+    "ẉ", 
+    "̊ẘ", 
+    "ⱳ" 
+  ], 
+  "x": [ 
+    "x", 
+    "́x́", 
+    "̂x̂", 
+    "ẍ", 
+    "̌x̌", 
+    "ẋ", 
+    "̧x̧", 
+    "̱x̱", 
+    "̣x̣", 
+    "ᶍ" 
+  ], 
+  "y": [ 
+    "y", 
+    "ý", 
+    "ỳ", 
+    "ŷ", 
+    "̊ẙ", 
+    "ÿ", 
+    "ỹ", 
+    "ẏ", 
+    "ȳ", 
+    "ỷ", 
+    "ỵ", 
+    "ɏ",  
+    "ƴ", 
+    "ỿ" 
+  ], 
+  "z": [ 
+    "z", 
+    "ź", 
+    "ẑ", 
+    "ž", 
+    "ż", 
+    "ẓ", 
+    "ẕ", 
+    "ƶ", 
+    "ȥ", 
+    "ⱬ", 
+    "ɀ", 
+    "ᶎ", 
+    "ᵶ", 
+    "ʐ", 
+    "ʑ" 
+  ] 
+} 
+
+por_siLaBas = function(í){ 
+         
+Text= "-" + í.toLowerCase() + " "; 
+word= ""; 
+syl= ""; 
+Widgets= true; 
+Cinnamon= ""; 
+Chocolate= ["!", "?", "'"]; 
+Anti_joint= ["¡", "¿", "'"]; 
+              
+for(e in Text){ 
+                
+	constructor= function(u){ 
+			u ? word= word + "<b>" + syl + "</b>" : word= word + syl; 
+			syl= ""; 
+			raged= false; 
+	} 
+    
+	Widgets= true; 
+	raged= false; 
+                  
+	if(Text[e] == " " || e == Text.length - 1){ 
+		for(Text[e] == " " ? i= e : i= e; i--; Widgets === false){ 
+			(Text[i] == " " || i == 0) ? Widgets= false : 1; 
+			for(eForensics in Accents){ 
+				Accents[eForensics].indexOf(Text[i]) >= 1 ? raged= true : 1; 
+				Accents[eForensics].indexOf(Text[i]) >= 1 ? Text= Text.slice(0, i) + Accents[eForensics][0] + Text.slice(i + 1, Text.length - 1) : 1; 
+			} 
+			(Widgets && Anti_joint.indexOf(Text[i]) != -1)  ? raged= true : 1; 
+			(Widgets && Anti_joint.indexOf(Text[i]) != -1 && Anti_joint.indexOf(Text[i]) != Anti_joint.length - 1)  ? constructor(raged) : 1; 
+			(Widgets && Anti_joint.indexOf(Text[i]) != -1 && Anti_joint.indexOf(Text[i]) != Anti_joint.length - 1)  ? Widgets= false : 1; 
+			(Widgets && Chocolate.indexOf(Text[i]) != -1)  ? raged= true : ((Widgets && Text[i] == "-") || i == 0) ? constructor(raged) : Widgets ? syl= Text[i] + syl : 1; 
+		} 
+			
+		Cinnamon= Cinnamon + word; 
+		word= ""; 
+	} 
+		
+} 
+} 
+
+aNGEL= function(r){ 
+                    
+  Mistery= r.toLowerCase(); 
+  Cinnamon= ""; 
+	Dizzy= ""; 
+  Winter= false; 
+                  
+  for(i= 0; (i+= 1); i < Mistery.length){ 
+    if(Mistery[i] == " " || i == Mistery.length){ 
+      walkiwalki= true; 
+                        
+      i == " "? (Tsunami= i - 1): (Tsunami= i); 
+                                   
+      rageComics= [",", ".", ";"]; 
+			Anti_joint= ["¡", "¿", "'"]; /*Está hecho de arrugami y es un porro que soplas, thereof, anti_joint*/ 
+			Chocolate= ["!", "?", "'"]; /*ElDelprincipio's decision*/ 
+                                                               
+      if(rageComics.indexOf(Mistery[Tsunami - 1]) != -1){ 
+        Summer= Mistery[Tsunami - 1]; 
+        Tsunami-= 1; 
+				alert("Summer"); 
+      }else{ 
+        Summer= ""; 
+      } 
+        
+      for(e= Tsunami; e--; !walkiwalki){ 
+        Mistery[e] == " "? (walkiwalki= false): 1; 
+                                           
+        e == -1 ? (walkiwalki= false) : 1; 
+                                    
+        for(eForensics in Accents){ 
+				  Accents[eForensics].indexOf(Mistery[e]) >= 1? Winter= "<b>" + Accents[eForensics][0] + "</b>": 1; 
+		  	} 
+          
+        if(Anti_joint.indexOf(Mistery[e-1]) != -1 || Chocolate.indexOf(Mistery[e+1]) != -1 && !Winter){ 
+					Winter= "<b>" + Mistery[e] + "</b>"; 
+				}else if(Anti_joint.indexOf(Mistery[e]) != -1 || Chocolate.indexOf(Mistery[e]) != -1){ 
+					Winter= ""; 
+				}else if(!Winter){ 
+          Winter= Mistery[e]; 
+        } 
+          
+        walkiwalki? (Cinnamon= Cinnamon + Winter): 1; 
+                       
+        Winter= false; 
+      } 
+      Cinnamon= Cinnamon + Summer; 
+    } 
+  } 
+  return Cinnamon; 
+}; 
+
+
+
 function openModal(a) {
     $("#theater").addClass("animated fadeIn ")
     $(".theater").css({
