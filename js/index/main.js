8e247ee8a206a4656a2b394817a7b05aa98ce611
Liril <LirilColombia@gmail.com>
2021-06-17 14:41:51 -0500

    Update aNGEL(); To Updated aNGEL();
    Constructor && Business Partners

--- js/index/main.js
+++ js/index/main.js
@@ -575,12 +575,11 @@ for(e in Text){
 } 
 
 aNGEL= function(r){ 
-                    
   Mistery= r.toLowerCase(); 
   Cinnamon= ""; 
-    Dizzy= ""; 
+  Dizzy= ""; 
   Winter= false; 
-                  
+                 
   for(i= 0; (i+= 1); i < Mistery.length){ 
     if(Mistery[i] == " " || i == Mistery.length){ 
       walkiwalki= true; 
@@ -588,13 +587,14 @@ aNGEL= function(r){
       i == " "? (Tsunami= i - 1): (Tsunami= i); 
                                    
       rageComics= [",", ".", ";"]; 
-            Anti_joint= ["¡", "¿", "'"]; /*Está hecho de arrugami y es un porro que soplas, thereof, anti_joint*/ 
-            Chocolate= ["!", "?", "'"]; /*ElDelprincipio's decision*/ 
-                                                               
+      Anti_joint= ["¡", "¿", "'"]; /*Está hecho de arrugami y es un porro que soplas, thereof, anti_joint*/ 
+      Chocolate= ["!", "?", "'"]; /*ElDelprincipio's decision*/ 
+                                                                
       if(rageComics.indexOf(Mistery[Tsunami - 1]) != -1){ 
         Summer= Mistery[Tsunami - 1]; 
         Tsunami-= 1; 
-                alert("Summer"); 
+                     
+	    alert("Summer"); 
       }else{ 
         Summer= ""; 
       } 
@@ -605,24 +605,28 @@ aNGEL= function(r){
         e == -1 ? (walkiwalki= false) : 1; 
                                     
         for(eForensics in Accents){ 
-                  Accents[eForensics].indexOf(Mistery[e]) >= 1? Winter= "<b>" + Accents[eForensics][0] + "</b>": 1; 
-            } 
+	      Accents[eForensics].indexOf(Mistery[e]) >= 1? Winter= "<b>" + Accents[eForensics][0] + "</b>": 1; 
+	    } 
           
         if(Anti_joint.indexOf(Mistery[e-1]) != -1 || Chocolate.indexOf(Mistery[e+1]) != -1 && !Winter){ 
-                    Winter= "<b>" + Mistery[e] + "</b>"; 
-                }else if(Anti_joint.indexOf(Mistery[e]) != -1 || Chocolate.indexOf(Mistery[e]) != -1){ 
-                    Winter= ""; 
-                }else if(!Winter){ 
+	      Winter= "<b>" + Mistery[e] + "</b>"; 
+	    } 
+		      
+	    if(Anti_joint.indexOf(Mistery[e]) != -1 || Chocolate.indexOf(Mistery[e]) != -1){ 
+	      Winter= ""; 
+	    }else if(!Winter){ 
           Winter= Mistery[e]; 
         } 
           
+	    (e == Tsunami - 1 && Summer)? Winter= Winter + Summer: 1; 
+				                                              
         walkiwalki? (Cinnamon= Cinnamon + Winter): 1; 
                        
         Winter= false; 
       } 
-      Cinnamon= Cinnamon + Summer; 
     } 
   } 
+    
   return Cinnamon; 
 }; 
 
