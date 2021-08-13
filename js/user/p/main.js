b27bbd8e2e9e1037b3f1daee431dcd876b750bb5
Liril <LirilColombia@gmail.com>
2021-08-13 16:25:12 -0500

    ¡Proyectos!
    Los proyectos son como repositorios de Git, para lo que los necesiten los estudiantes y profesores de Liril.
    
    I love you a lottt GitHub!
    
    Constructor && Business Partners

---
+++ js/user/p/main.js
@@ -0,0 +1,500 @@
+function openModal(a) {
+    $("#theater").addClass("animated fadeIn ")
+    $(".theater").css({
+        "display": "block"
+    })
+    $this = a.closest('.story')
+    var source = a.attr('src');
+    $('.theater #bigPic').attr('src', source);
+
+    var user = '<a href="user.html">Walter White</a><br><span class="time">24 de Octubre a las 0:30 am</span>'; 
+    $('.theater .comments .info .username').html(user); 
+
+    var time = a.closest('.story').find(".time").html();
+    $('.theater .comments .info .time').html(time);
+
+    var title = a.closest('.story').find(".title").html();
+    $('.theater .comments .title').html(title);
+
+    if (a.closest('.story').find(".options .bookmark").hasClass("true")) {
+        $('.theater .comments .options .bookmark').addClass("true");
+    } else {
+        $('.theater .comments .options .bookmark').removeClass("true");
+    }
+    if (a.closest('.story').find(".options .star").hasClass("true")) {
+        $('.theater .comments .options .star').addClass("true");
+    } else {
+        $('.theater .comments .options .star').removeClass("true");
+    }
+
+    var pic = "../resources/images/white.jpg"; 
+    $('.theater .comments .info #pic').attr('src', pic); 
+
+    var ref = a.closest('.story').find(".options ul a").attr('href');
+    $('.theater .comments .options ul a').attr('href', ref);
+
+    if($("#bigPic").width()<=$("#bigPic").height()){
+        $("#bigPic").css({ "width":"100%"})
+    }else{
+
+    }
+      $(".more").mCustomScrollbar({theme: 
+        "minimal-dark", 
+        autoExpandScrollbar: true,
+        scrollInertia: 100});
+      
+        $("#theater").height($(window).height() - 40);
+
+    $("#picContainer").width($("#bigPic").width());
+    $("#theater .comments").height($("#theater").height() - 40);
+    $("#theater .comments .more").height($("#theater .comments").height() - $("#theater .info").outerHeight(true) - $("#theater .comments .title").outerHeight(true) - $("#theater .comments .options").outerHeight(true));
+    $("#theater").width("90%");
+    responsive()
+}
+
+function closeModal() {
+    if ($('.theater .comments .options .bookmark').hasClass("true")) {
+        $this.find('.options .bookmark').addClass("true");
+    } else {
+        $this.find('.options .bookmark').removeClass("true");
+    }
+    if ($('.theater .comments .options .star').hasClass("true")) {
+        $this.find('.options .star').addClass("true");
+    } else {
+        $this.find('.options .star').removeClass("true");
+    }
+    $(".theater").css({
+        "display": "none"
+    })
+}
+$(document).on("ready",function(e){
+    $('.chats').click(function(e){ 
+        aa= e.target; 
+        ii= false; 
+        while(!!aa.parentElement && !ii){ 
+            (!!aa.parentElement.getAttribute("class") && aa.parentElement.getAttribute("class").indexOf("chat ") != -1)? ii = true: 1; 
+            aa= aa.parentElement; 
+        }
+        if(!ii){ 
+            $(this).hide(); 
+            $(document.elementFromPoint(e.clientX, e.clientY)).trigger("click"); 
+            $(this).show(); 
+        } 
+    }); 
+    $("html").click(function() {
+        $(".wrapper").removeClass("visible")
+        $("#right-menu .index-arrow").removeClass("open");
+        $("#profileSettings").removeClass("open");
+
+
+        if ($("#profileSettings").hasClass("open")) {
+            if($("#profileSettings").hasClass("openedStore")){
+            $('.knob').trigger(
+                'configure', {
+                    "fgColor": "#2ecc71"
+                }
+            );
+        }
+        else{
+            $('.knob').trigger(
+                'configure', {
+                    "fgColor": "#2ecc71"
+                }
+            );
+        }
+    }
+        else{
+            if($("#profileSettings").hasClass("openedStore")){
+
+            $('.knob').trigger(
+                'configure', {
+                    "fgColor": "#fff"
+                }
+            );
+        }
+        else{
+            $('.knob').trigger(
+                'configure', {
+                    "fgColor": "#2ecc71"
+                }
+            );
+        }
+        }
+
+      
+
+
+e.stopPropagation() 
+
+
+});
+
+$('#profileSettings').click(function(i){
+    i.stopPropagation()
+   
+}); 
+});
+$(document).keypress(function (e) {
+if(!$("textarea").is(":focus") && !$("input").is(":focus")){
+    if (e.keyCode == 108) {
+        $(".current").find('.options .star').toggleClass("true");
+    }
+    if (e.keyCode == 98) {
+        $(".current").find('.options .bookmark').toggleClass("true");
+    }
+    if (e.keyCode == 102) {
+        openModal($(".current .pic"))
+    }
+    var forward;
+    if (e.keyCode == 106) {
+        forward = true;
+    } else if (e.keyCode == 107) {
+        forward = false;
+    } else {
+        return true;
+    }
+
+
+    if (!$(".story.current").length) {
+        $(".story:first").addClass("current");
+    }
+
+    if ($(".story.current").length) {
+        var current = $(".story.current");
+        var next;
+        if (forward) {
+            next = current.next(".story")
+        } else {
+            next = current.prev(".story");
+        }
+        if (next.length) {
+            current.removeClass("current");
+            next.addClass("current");
+        } else {
+            loadMore();
+        }
+    }
+
+    $("html, body").animate({
+        scrollTop: $(".current").offset().top - 56
+    }, 400);
+    }
+});
+var c = 0
+var $this
+$(window).on("load", function () {
+    responsive();
+    $("#chat ul li").on("click",function(){
+        if($(this).attr('class').split(' ')[2] == null){
+            $(this).addClass( c.toString() );
+            newChat($(this), c);
+            c++
+        }else{
+            $dat =$(".chats .chat."+$(this).attr('class').split(' ')[2])
+            if(! $dat.hasClass("open")){
+                prependClass($dat,"open");  
+            }
+            $(".chats .chat."+$(this).attr('class').split(' ')[2]+" .newMessage textarea").focus();
+        }
+    }
+
+
+        )
+
+  
+    $("#chats .chats ").on("mouseover mouseout", ".chatTitle", function() {
+        $( this ).find(".close").toggleClass( "visible" );
+      });
+    $( ".chatTitle .close" ).on("click",
+      function() {
+        destroyChat($(this));
+      }
+    );
+    $("#chats .chats ").on("click", ".chatTitle", function() {
+         $( this ).closest(".chat").toggleClass("open");
+       $( this ).parent().find($(".newMessage textarea")).focus()
+
+      });
+    $("#chats .chats ").on("click", ".chatTitle .close", function() {
+         destroyChat($( this ))
+      });
+    $(".story").on("click", function () {
+        $(".current").removeClass("current");
+        $(this).addClass("current")
+    })
+    $("li").prop("title", function () {
+        return $(this).find("p").text();
+    });
+
+    $('.knob').trigger(
+        'configure', {
+            "fgColor": "#2ecc71"
+        }
+    );
+
+    $(".star").on("click", function () {
+        $(this).toggleClass("true")
+    });
+    $(".bookmark").on("click", function () {
+        $(this).toggleClass("true")
+    });
+
+    $("#dots").on("click", function () {
+        $("#Store").toggleClass("open");
+        $("#index-trigger").children().toggleClass("openedStore");
+        $("#right-menu #dots").children().toggleClass("openedStore");
+        $("#right-menu").children().toggleClass("openedStore");
+        $("#Store #actualStore").height($(window).height() - $("header").height() - 21);
+        $("#Store #actualStore #storeSection").width($(window).width() - $("#Store #actualStore #storeList").width() - 87);
+        $("#Store #actualStore #storeSection").height($("#Store #actualStore").height() - 80);
+        
+            $('.knob').trigger(
+                'configure', {
+                    "fgColor": "#fff"
+                }
+            );
+        
+        if ($("#Store").hasClass("open")) {
+            $("header").addClass("open");
+           
+                 $('.knob').trigger(
+                'configure', {
+                    "fgColor": "#fff"
+                
+            
+            })
+        } else {
+           $("header").removeClass("open");
+           $('.knob').trigger(
+                   'configure', {
+                       "fgColor": "#2ecc71"
+                   }
+               );
+           }
+          
+               
+            
+        });
+    
+    $("#profilePic").on("click", function () {
+        openProfilePicModal($(this))
+    }); 
+    
+    $("#gallery img").on("click", function () {
+        openModal($(this));
+        responsive(); 
+                $('#target').tooltip()
+
+    });
+
+    $("#close").on("click", function () {
+
+        closeModal()
+    });
+
+    $("#wrapper").on("click", function () {
+        closeModal()
+    });
+   
+document.onkeydown = function (evt) {
+        evt = evt || window.event;
+        if (evt.keyCode == 27) {
+            if($("#theater").css("display")== "none"){
+             destroyChat($(document.activeElement))
+        }else{
+                        closeModal();
+
+        }
+            }
+    };
+   
+     $(".chat").on("click", function(){})
+
+        if ($("#profileSettings").hasClass("open") && $("#Store").hasClass("open")) {
+            $('.knob').trigger(
+                'configure', {
+                    "fgColor": "#2ecc71"
+                }
+            );
+        }
+$("#chats .chats ").on("click", ".chat", function(ev) {
+        if(getSelectionText()  == ""){
+        $(this).find("textarea").focus();
+}
+});
+    $("#profileTrigger").on("click", function (e) {
+        $(".wrapper").toggleClass("visible")
+        $("#right-menu .index-arrow").toggleClass("open");
+        $("#profileSettings").toggleClass("open");
+
+
+        if ($("#profileSettings").hasClass("open")) {
+            if($("#profileSettings").hasClass("openedStore")){
+            $('.knob').trigger(
+                'configure', {
+                    "fgColor": "#2ecc71"
+                }
+            );
+        }
+        else{
+            $('.knob').trigger(
+                'configure', {
+                    "fgColor": "#2ecc71"
+                }
+            );
+        }
+    }
+        else{
+            if($("#profileSettings").hasClass("openedStore")){
+
+            $('.knob').trigger(
+                'configure', {
+                    "fgColor": "#fff"
+                }
+            );
+        }
+        else{
+            $('.knob').trigger(
+                'configure', {
+                    "fgColor": "#2ecc71"
+                }
+            );
+        }
+        }
+
+      
+
+
+e.stopPropagation() 
+
+
+
+
+    });
+$("#chats .chats").on("keypress", ".chat .chatBox .newMessage textarea", function (ev) {
+    sizeMessages($(this));
+    if(ev.keyCode == 13 &&  $(this).val() != 0 && !ev.shiftKey){
+      ev.preventDefault();
+      var content = this.value;                
+       $(this).closest(".chat").find(".messages").append("<p class='sent'>" + content + "</p>")
+       $(this).val("")
+       scrollBottom($(this).closest(".chat").find(".chatBox .messages"))
+  }
+ })
+$("#chats .chats").on("keyup", ".chat .chatBox .newMessage textarea", function (ev) {
+    sizeMessages($(this));
+ 
+ })
+$("#chats .chats").on("keydown", ".chat .chatBox .newMessage textarea", function (ev) {
+    sizeMessages($(this));
+ 
+ })
+});
+
+$(window).on("resize", function(){
+    responsive();
+})
+function responsive() {
+    $("#file_expl").height($(window).height() - $("header").outerHeight() - $("#root").outerHeight());
+     $("#picContainer").width($("#theater").width()- $(".comments").outerWidth())
+    $("#Store #actualStore").height($(window).height() - $("header").height() - 21);
+        $("#Store #actualStore #storeSection").width($(window).width() - $("#Store #actualStore #storeList").width() - 87);
+        $("#Store #actualStore #storeSection").height($("#Store #actualStore").height() - 80);
+     $("#picContainer").width($("#bigPic").width());
+
+$("#theater .comments").height($("#theater").height() - 40);
+    $("#theater .comments .more").height($("#theater .comments").height() - $("#theater .info").outerHeight(true) - $("#theater .comments .title").outerHeight(true) - $("#theater .comments .options").outerHeight(true));
+    $("#theater").height($(window).height() - 40);
+    
+        $("#sidebar").height($(window).height() - 46);
+        $("#sidebar #container").height(($(window).height() - $("#sidebar #search").outerHeight() - 46));
+        $("#sidebar #chat").height(($(window).height() - $("#sidebar #search").outerHeight() - 46));
+        if ($("#resizeBottom").height() > ($(window).height() - $("#sidebar #search").outerHeight() - 51)) {
+            $("#resizeBottom").css({
+                "height": "50%"
+            })
+        }
+
+        if (parseInt($(window).width() - $("#sidebar").outerWidth()) <= 600) {
+            $("#feed .story").css({
+                "padding": "10px 0"
+               
+            })
+            $("#feed .story .info").css({
+                "text-align": "center"
+            })
+
+        } else {
+            $("#feed .story").css({
+                "padding": "10px"
+            })
+            $("#feed .story .info").css({
+                "text-align": "left"
+            })
+        }
+        $("#chats .chats").width($(window).width() - $("#sidebar").outerWidth() -10);
+        $("#biography").width($(window).width() - $("#sidebar").outerWidth());
+        $("#file_expl #preview").width($(window).width() - $("#file_expl #files").width());
+
+ }
+
+ function newChat(a, b){
+   
+        $(".chats").prepend("<div class='chat open "+b.toString()+"'><div class='chatTitle button'><p class='user'>"+ a.find("p").text() +"</p><span class='close'></span></div><div class='chatBox'><div class='messages'></div><div class='newMessage'><textarea rows='1'/><div class='emoticon'></div></div></div>");
+        $(".chats .chat:first-child .newMessage textarea").focus();
+        $('textarea').textareaAutoSize();
+
+}
+function destroyChat(a){
+    var i = a.closest(".chat").hasClass("open") ? a.closest(".chat").attr('class').split(' ')[2] : a.closest(".chat").attr('class').split(' ')[1]
+    $("#chat ul li."+i).removeClass(i)
+    a.closest(".chat").remove();
+    $(".chats .chat:first-child .newMessage textarea").focus();
+
+}
+function getSelectionText() {
+    var text = "";
+    if (window.getSelection) {
+        text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+        text = document.selection.createRange().text;
+    }
+    return text;
+}
+function prependClass(sel, strClass) {
+    var $el = jQuery(sel);
+
+    /* prepend class */
+    var classes = $el.attr('class');
+    classes = strClass +' ' +classes;
+    $el.attr('class', classes);
+}
+function getCaret(el) {
+  if (el.selectionStart) {
+     return el.selectionStart;
+  } else if (document.selection) {
+     el.focus();
+
+   var r = document.selection.createRange();
+   if (r == null) {
+    return 0;
+   }
+
+    var re = el.createTextRange(),
+    rc = re.duplicate();
+    re.moveToBookmark(r.getBookmark());
+    rc.setEndPoint('EndToStart', re);
+
+    return rc.text.length;
+  }  
+  return 0;
+}
+function sizeMessages(a){
+    CBH= a.closest(".chat").find(".chatBox").height()
+    NMH = a.closest(".chat").find(".chatBox .newMessage" ).outerHeight()
+    a.closest(".chat").find(".chatBox .messages").height(CBH - NMH - 8);
+    a.closest(".chat").find(".chatBox .messages").css({bottom: NMH})
+}
+function scrollBottom(d){
+    d.scrollTop(d.prop("scrollHeight"));
+}
