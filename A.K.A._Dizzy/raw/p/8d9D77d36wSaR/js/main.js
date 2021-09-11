ae122309bc03737e26a9435598a279a905172aae
Liril <LirilColombia@gmail.com>
2021-09-11 16:01:29 -0500

    Algunas Propiedades De Mis Escritos
    Constructor && Business Partners

---
+++ A.K.A._Dizzy/raw/p/8d9D77d36wSaR/js/main.js
@@ -0,0 +1,92 @@
+getDedications= function(language, print){ 
+	s= 0; 
+	Name= []; 
+	Name[s]= 0; 
+	n= ""; 
+	name= document.getElementsByTagName("input")[0].value; 
+	syl= ""; 
+	Syl= []; 
+	i= 0; 
+	text= document.getElementsByTagName("textarea")[0].value.toLowerCase().replaceAll("\n", " "); 
+                    
+	for(e in text){ 
+		if(text[e] != "-" && text[e] != " " && parseInt(e) != text.length - 1){ 
+			syl+= text[e]; 
+		}else{ 
+			parseInt(e) == text.length - 1? syl+= text[e]: 3231; 
+
+			Syl[i]= syl; 
+			i++; 
+			     
+			text[e] == " "? syl= "": 1; 
+		} 
+	} 
+      
+	names= function(){ 
+		for(u in name){ 
+			if(name[u] != " "){ 
+				for(r in Alphabets[language]){ 
+					for(eForensics in Alphabets[language][r]){ 
+						parseInt(eForensics) != 0? name[u].toLowerCase() == Alphabets[language][r][eForensics][0]? (name[parseInt(u) - 1] == "'" || name[parseInt(u) + 1] == "'")? Name[s]+= Alphabets[language][r][0] + Alphabets[language][r][0] + Alphabets[language][r][0] * Alphabets[language][r][eForensics][1]: Name[s]+= Alphabets[language][r][0] + Alphabets[language][r][0] * Alphabets[language][r][eForensics][1]:17288: 1981; 
+					} 
+				} 
+			}else{ 
+				s++; 
+				Name[s]= 0; 
+			} 
+		} 
+	} 
+      
+	dedications= function(){ 
+		names(); 
+		dedication= ""; 
+		naM= 0; 
+                
+		for(q in Syl){ 
+			sT= parseInt(q); 
+			nuMber= 0; 
+                       
+			dedication+= Syl[sT] + " "; 
+                               
+			for(er_R in Name){ 
+				nuMber+= Name[er_R]; 
+				final_nuMber= nuMber + sT; 
+                                           
+				while(final_nuMber >= Syl.length){ 
+					final_nuMber-= Syl.length; 
+				} 
+                  
+				dedication+= Syl[final_nuMber] + " "; 
+			} 
+              
+			print.value= print.value + dedication + "\n"; 
+                            
+			dedication= ""; 
+		} 
+          
+        print.value= print.value + "//////////////////////////////////////////////////////////" + "\n\n"; 
+	} 
+      
+	dedications(); 
+} 
+$(document).on("ready", function(){ 
+	$(".form-group").on("submit", function (ev) {
+	  ev.preventDefault();
+	});
+	$(".getSp").click(function() { 
+	    getDedications("español", document.getElementsByTagName("textarea")[1]); }); 
+	$(".getEn").click(function() { 
+	    getDedications("english", document.getElementsByTagName("textarea")[2]); }); 
+	$(".tidyName").click(function() { 
+	    $(".results")[0].value= ""; 
+	    $(".resultsEn")[0].value= ""; 
+	    $(".name")[0].value= ""; 
+	     $(".name")[0].focus(); 
+	  }); 
+	 $(".tidyText").click(function() { 
+	    $(".results")[0].value= ""; 
+	    $(".resultsEn")[0].value= ""; 
+	    $(".dedications")[0].value= ""; 
+	    $(".dedications")[0].focus(); 
+	  }); 
+})
