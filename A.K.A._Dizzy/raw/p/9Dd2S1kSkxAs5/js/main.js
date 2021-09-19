d318d3e643ed24d889238390955354f7c2cc7757
Liril <LirilColombia@gmail.com>
2021-09-19 06:56:12 -0500

    $$$(document).on("keypress keyup", function(){(function(){})()});
    Constructor && Business Partners

--- A.K.A._Dizzy/raw/p/9Dd2S1kSkxAs5/js/main.js
+++ A.K.A._Dizzy/raw/p/9Dd2S1kSkxAs5/js/main.js
@@ -12,9 +12,9 @@ return [[b, i.replaceAll(" ", "").length], i];
 
 document.addEventListener("DOMContentLoaded", function(){ 
     document.getElementById("ñ").checked? Accents= ÑAccents: Accents= NAccents; 
-    document.addEventListener("keypress", function(){ 	(function(){document.querySelector("#Sara p").outerHTML= "<p style= 'font-size: 15px; display: inline-block; '>" + contr( document.getElementsByTagName("textarea")[0].value )[0][0] + " Palabras. " + "</br>" + contr( document.getElementsByTagName("textarea")[0].value )[0][1] + " Caracteres (Sin espacios)." + "</p>"; })() }); 
+    document.addEventListener("keypress", function(){ 	(function(){document.querySelector("#Sara p").outerHTML= "<p style= 'font-size: 15px; display: inline-block; '>" + contr( document.getElementsByTagName("textarea")[0].value )[0][0] + " Palabras. " + "    " + contr( document.getElementsByTagName("textarea")[0].value )[0][1] + " Caracteres (Sin espacios)." + "</p>"; })() }); 
 
-    document.addEventListener("keyup", function(){ 	(function(){document.querySelector("#Sara p").outerHTML= "<p style= 'font-size: 15px; display: inline-block; '>" + contr( document.getElementsByTagName("textarea")[0].value )[0][0] + " Palabras. " + "</br>" + contr( document.getElementsByTagName("textarea")[0].value )[0][1] + " Caracteres (Sin espacios)." + "</p>"; })() }); 
+    document.addEventListener("keyup", function(){ 	(function(){document.querySelector("#Sara p").outerHTML= "<p style= 'font-size: 15px; display: inline-block; '>" + contr( document.getElementsByTagName("textarea")[0].value )[0][0] + " Palabras. " + "    " + contr( document.getElementsByTagName("textarea")[0].value )[0][1] + " Caracteres (Sin espacios)." + "</p>"; })() }); 
 
     document.addEventListener("keydown", function(i){ 
     	i.keyCode == 16? por_siLaBas(document.getElementById("dieded").value): 1; 
