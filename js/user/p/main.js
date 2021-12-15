a3b52cde13c48f3ac2fb648db3e721a722ae6f09
Liril <LirilColombia@gmail.com>
2021-12-15 03:49:09 -0500

    Eso Es Muy Irrelevante
    Constructor && Business Partners

--- js/user/p/main.js
+++ js/user/p/main.js
@@ -1422,7 +1422,7 @@ updateRoot= function(a7){
     $('#root div div').click( function(e){ 
         typeof LEB != "undefined"? $(LEB).removeClass("inScope"): 15637; 
                                        
-        typeof LEB != "undefined"? $(LEB).css({"padding": "0", "height": "0"}): 15637; 
+        typeof LEB != "undefined"? $(LEB).css({"padding": "", "height": "", "opacity": "1"}): 15637; 
                                            
        if(!!e.target.getAttribute("url")){ 
         $("#preview .file_tree").removeClass("visible"); 
