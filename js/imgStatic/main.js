4935e8984b1f77e90e4addf63e57287db2b77360
Liril <LirilColombia@gmail.com>
2022-02-20 15:14:28 -0500

    |
    Constructor && Business Partners

--- js/imgStatic/main.js
+++ js/imgStatic/main.js
@@ -45,6 +45,10 @@ document.addEventListener("DOMContentLoaded", function(){
     }) 
 	$(".carr img").each(function(){ 
 		$(this)[0].src.indexOf(window.location.pathname.slice(window.location.pathname.lastIndexOf("/") + 1)) != -1? $(".carr").scrollLeft($(".carr").width() * $(this).parent().index()) : 123
-	})
+	}); 
+    
+    rf= $(".carr")
+    Math.round($(rf)[0].scrollLeft / $(rf).width()) == $(rf).find(".pic").length - 1? (function(){rf.parent().find(".nav_arrow.right").addClass("disabled"); $("#theater").find(".nav_arrow.right").addClass("disabled")})(): (function(){rf.parent().find(".nav_arrow.right").removeClass("disabled"); $("#theater").find(".nav_arrow.right").removeClass("disabled")})(); 
+    $(rf)[0].scrollLeft == 0? (function(){rf.parent().find(".nav_arrow.left").addClass("disabled"); $("#theater").find(".nav_arrow.left").addClass("disabled");})(): (function(){rf.parent().find(".nav_arrow.left").removeClass("disabled"); $("#theater").find(".nav_arrow.left").removeClass("disabled")})(); 
 })
 
