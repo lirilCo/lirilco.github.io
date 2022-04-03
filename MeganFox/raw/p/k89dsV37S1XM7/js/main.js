1e670831d289448c31cc0ea59179e843ce177766
Liril <LirilColombia@gmail.com>
2022-04-03 18:52:49 -0500

    Don't Open Ever - set
    Constructor && Business Partners

--- MeganFox/raw/p/k89dsV37S1XM7/js/main.js
+++ MeganFox/raw/p/k89dsV37S1XM7/js/main.js
@@ -10,6 +10,12 @@ ________________________________________________________________________________
 ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
 `; 
 
+$.fn.selectRange= function(start, end) {if(!end) end= start; return this.each(function() {if(this.setSelectionRange){this.focus(); this.setSelectionRange(start, end); }else if(this.createTextRange){var range= this.createTextRange(); range.collapse(true); range.moveEnd('character', end); range.moveStart('character', start); range.select(); }});}; 
+
+set= function(z){ 
+	$("#grid").selectRange((!!z[0] || !z[0]? (100 * z[1] + z[1] + z[0]): (1 || 2 || 3))); 
+} 
+
 $(function(){
 	$("#grid").val(map.replaceAll("|", "").slice(104, -104)); 
 
