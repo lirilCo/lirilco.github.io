9ccf8fcd7427edb3c94726ce44b71edeab9ed7cf
Liril <LirilColombia@gmail.com>
2022-04-08 21:59:16 -0500

    minesweeper - Continue();
    Constructor && Business Partners

--- L/raw/p/d8E461R2s91c/js/main.js
+++ L/raw/p/d8E461R2s91c/js/main.js
@@ -15,21 +15,54 @@ AntiEngineerer= function(x, y){
 }; 
    
 function sweep(coor, s){ 
-	for(e= coor[0]; e >= coor[0] - s; e--) 
-		for(a= coor[1] - s; a <= coor[1] + s; a++) 
-			typeof AntiEngineerer(e, a) != "undefined"? (function(){AntiEngineerer(e, a).checked= true; AntiEngineerer(e, a).mined? AntiEngineerer(e, a).setAttribute("disabled", true): 1; })(): 1; 
-	for(e= coor[0] + 1; e <= coor[0] + s; e++) 
-		for(a= coor[1] - s; a <= coor[1] + s; a++) 
-			typeof AntiEngineerer(e, a) != "undefined"? (function(){AntiEngineerer(e, a).checked= true; AntiEngineerer(e, a).mined? AntiEngineerer(e, a).setAttribute("disabled", true): 1; })(): 1; 
+	coo= coor; 
+	for(u= coor[0]; u >= coor[0] - s; u--) 
+		for(y= coor[1] - s; y <= coor[1] + s; y++) 
+			typeof AntiEngineerer(u, y) != "undefined"? (function(){AntiEngineerer(u, y).checked= true; AntiEngineerer(u, y).mined? (function(){AntiEngineerer(u, y).setAttribute("disabled", true); Demine(); })(): 1; })(): 1; 
+	for(r= coor[0] + 1; r <= coor[0] + s; r++) 
+		for(w= coor[1] - s; w <= coor[1] + s; w++) 
+			typeof AntiEngineerer(r, w) != "undefined"? (function(){AntiEngineerer(r, w).checked= true; AntiEngineerer(r, w).mined? (function(){AntiEngineerer(r, w).setAttribute("disabled", true); Demine(); })(): 1; })(): 1; 
 }; 
-  
+   
+function Demine(){ 
+	throw new Error("Demining..."); 
+}; 
+   
+function Continue(){
+	coor= coo; 
+	admited= true;
+	right? w++: y++; 
+	s= 2
+	if(!right){
+		for(u= u; ((u >= coor[0] - s) || admited); u--){
+			for(y= (admited? y: (coor[1] - s)); y <= coor[1] + s; y++){
+				admited= false; 
+				typeof AntiEngineerer(u, y) != "undefined"? (function(){AntiEngineerer(u, y).checked= true; AntiEngineerer(u, y).mined? (function(){AntiEngineerer(u, y).setAttribute("disabled", true); Demine(); })(): 1; })(): 1; 
+			} 
+		} 
+	}
+	right= true; 
+	for(r= ((typeof r != "undefined")? r: (coor[0] + 1)); ((r <= coor[0] + s) || admited); r++){
+		for(w= (admited? w: (coor[1] - s)); w <= coor[1] + s; w++){
+			admited= false; 
+			typeof AntiEngineerer(r, w) != "undefined"? (function(){AntiEngineerer(r, w).checked= true; AntiEngineerer(r, w).mined? (function(){AntiEngineerer(r, w).setAttribute("disabled", true); Demine(); })(): 1; })(): 1; 
+		}
+	}
+	r= "undefined"; 
+	u= "undefined"; 
+	y= "undefined"; 
+	w= "undefined"; 
+	right= false; 
+}; 
+
+var right= false; 
 document.addEventListener("DOMContentLoaded", function(){ 
 	populate= function(w, h){ 
 		for(e= 1; e <= h; e++){ 
-		    document.getElementsByTagName("tbody")[document.getElementsByTagName("tbody").length - 1].innerHTML+= "<div></div>"; 
-		    for(i= 1; i < w; i++){ 
-		        document.getElementsByTagName("tbody")[document.getElementsByTagName("tbody").length - 1].children[document.getElementsByTagName("tbody")[document.getElementsByTagName("tbody").length - 1].children.length - 1].innerHTML+= "<input></input>"; 
-		    	document.getElementsByTagName("tbody")[document.getElementsByTagName("tbody").length - 1].children[document.getElementsByTagName("tbody")[document.getElementsByTagName("tbody").length - 1].children.length - 1].children[document.getElementsByTagName("tbody")[document.getElementsByTagName("tbody").length - 1].children[document.getElementsByTagName("tbody")[document.getElementsByTagName("tbody").length - 1].children.length - 1].children.length - 1].type= "checkbox"; 
+		    document.getElementsByTagName("tbody")[0].innerHTML+= "<div></div>"; 
+		    for(i= 1; i <= w; i++){ 
+		        document.getElementsByTagName("tbody")[0].children[document.getElementsByTagName("tbody")[0].children.length - 1].innerHTML+= "<input></input>"; 
+		    	document.getElementsByTagName("tbody")[0].children[document.getElementsByTagName("tbody")[0].children.length - 1].children[document.getElementsByTagName("tbody")[0].children[document.getElementsByTagName("tbody")[0].children.length - 1].children.length - 1].type= "checkbox"; 
 		    } 
 		} 
 
@@ -42,15 +75,15 @@ document.addEventListener("DOMContentLoaded", function(){
         	} 
         } 
 
-        for(a= 0; a <= document.querySelectorAll("table tbody div")[document.querySelectorAll("table tbody div").length - 1].children.length; a++){ 
+        for(a= 0; a <= document.querySelectorAll("table tbody div")[document.querySelectorAll("table tbody div").length - 1].children.length - 1; a++){ 
 			document.querySelectorAll("table tbody div")[a].style.height= document.querySelectorAll("table tbody div input")[0].clientHeight + "px"; 
 		} 
 
 		for(a= 0; a <= document.querySelectorAll("table tbody div input").length - 1; a++){ 
     		document.querySelectorAll("table tbody div input")[a].mined= parseInt(Math.random() * 9) == 1? true: false; 
-    		document.querySelectorAll("table tbody div input")[a].addEventListener("change", function(e){/*console.log(["Mined: ", this.mined]); */ sweep([Array.prototype.indexOf.call(e.target.parentElement.children, e.target), Array.prototype.indexOf.call(e.target.parentElement.parentElement.children, e.target.parentElement)], 2); cosaParaengineerer= Array.prototype.indexOf.call(e.target.parentElement.parentElement.querySelectorAll("input"), e.target)}); 
+    		document.querySelectorAll("table tbody div input")[a].addEventListener("change", function(e){/*console.log(["Mined: ", this.mined]); */ cosaParaengineerer= Array.prototype.indexOf.call(e.target.parentElement.parentElement.querySelectorAll("input"), e.target); sweep([Array.prototype.indexOf.call(e.target.parentElement.children, e.target), Array.prototype.indexOf.call(e.target.parentElement.parentElement.children, e.target.parentElement)], 2); }); 
 		} 
 	}; 
        
-	populate(10, 10); 
+	populate(20, 20); 
 }); 
